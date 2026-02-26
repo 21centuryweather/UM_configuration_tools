c4fd93a9c597e0e7d5d985fddd99fc906ba970de
Paul Gregory <pag548@gadi-login-02.gadi.nci.org.au>
2026-02-26 11:53:58 +1100

    UPDATES : Solved iris load issue with 64 bit precision problems.           Re-writing regional_ocean_mesh to align with h grid values

--- scripts/iris_load_primitive.py
+++ scripts/iris_load_primitive.py
@@ -5,19 +5,83 @@
 import iris
 from iris.fileformats._ff import FF2PP
 from pathlib import Path
+from iris.fileformats.pp_load_rules import convert
+import numpy as np
+
+def regular_points(zeroth, step, count):
+    """Make an array of regular points.
+
+    Create an array of `count` points from `zeroth` + `step`, adding `step` each
+    time. In float32 if this gives a sufficiently regular array (tested with
+    points_step) and float64 if not.
+
+    Parameters
+    ----------
+    zeroth : number
+        The value *prior* to the first point value.
+    step : number
+        The numeric difference between successive point values.
+    count : number
+        The number of point values.
+
+    Notes
+    -----
+    This function does maintain laziness when called; it doesn't realise data.
+    See more at :doc:`/userguide/real_and_lazy_data`.
+    """
+
+    def make_steps(dtype: np.dtype):
+        start = np.add(zeroth, step, dtype=dtype)
+        steps = np.multiply(step, np.arange(count), dtype=dtype)
+        return np.add(start, steps, dtype=dtype)
+
+    points = make_steps(np.float64)
+    
+    return points
+
+def replace_lat_lon(cube,filename:
+    """
+    Replace the 32-bit lat and lon points of input cube with 64 bit points
+    contructed from the PP field of the input cube
+    """
+
+    field, = FF2PP(filename), read_data=False)
+
+    bdx = field.bdx
+    bzx = field.bzx
+    lbnpt= field.lbnpt
+
+    bdy = field.bdy
+    bzy = field.bzy
+    lbrow = field.lbrow
+
+    lon_points = regular_points(bzx, bdx, lbnpt)
+    lat_points = regular_points(bzy, bdy, lbrow)
+
+    cube.coord('longitude').points = lon_points
+    cube.coord('latitude').points = lat_points
+
+    return cube
 
 
 ANCIL_DIR = Path('/scratch/gb02/pag548/cylc-run/rCM3-test-UM-ancil/share/data/ancils/Lismore/d1100')
 MASK = ANCIL_DIR / 'qrparm.mask'
-breakpoint()
-mask = iris.load(MASK)
+#breakpoint()
+mask, = iris.load(MASK)
 
-ff2pp = FF2PP(MASK, read_data=False)
+ff2pp, = FF2PP(MASK, read_data=False)
 
-fields = list(ff2pp)
+bdx = ff2pp.bdx
+bzx = ff2pp.bzx
+lbnpt= ff2pp.lbnpt
 
-bdx = fields[0].bdx
-bzx = fields[0].bzx
-lbnpt= fields[0].lbnpt
+bdy = ff2pp.bdy
+bzy = ff2pp.bzy
+lbrow = ff2pp.lbrow
 
 # Note these are read as 64 bit floats
+
+lon_points = regular_points(bzx, bdx, lbnpt)
+lat_points = regular_points(bzy, bdy, lbrow)
+
+mask = replace_lat_lon(mask,ff2pp)
--- scripts/regrid_regional_ocean_mesh.py
+++ scripts/regrid_regional_ocean_mesh.py
@@ -17,7 +17,7 @@ def add_options(parser):
     """
     Process the command-line arguments
     """
-    parser.description = 'Plot the memory usage of the Unified Model I/O server'
+    parser.description = 'Regrid a MOM6 ocean mesh onto a regular grid with the same bounds.\n The user specifies the output resolution'
 
     parser.add_argument('-f','--input-ocean-mesh-file', 
         dest='ocean_file', 
@@ -73,87 +73,7 @@ def get_bounds(ocn_ds):
     return bounds
 
 
-def build_grid(bounds, nlat):
-    """
-    Build an esmpy (ESMF) grid object in memory using Kieran's method at https://gist.github.com/kieranricardo/eb98f76235255efff800d28c2442e5c3
-    """
-    nlon = nlat
-    lon0, lon1 = bounds['lon_min'],bounds['lon_max']
-    lat0, lat1 = bounds['lat_min'],bounds['lat_max']
-
-    max_index = np.array([nlon, nlat])
-    grid = esmpy.Grid(max_index, num_peri_dims=1, staggerloc=[esmpy.StaggerLoc.CENTER])
-
-    dlat = (lat1 - lat0) / nlat
-    lat = (np.arange(nlat) * dlat) + lat0 + 0.5 * dlat
-
-    dlon = (lon1 - lon0) / nlon
-    lon = (np.arange(nlon) * dlon) + lon0 + 0.5 * dlon
-
-    grid.get_coords(0)[:] = lon[:, None]
-    grid.get_coords(1)[:] = lat[None, :]
-
-    # corner 
-    grid.add_coords([esmpy.StaggerLoc.CORNER])
-
-    grid_lon_corner = grid.get_coords(0, staggerloc=esmpy.StaggerLoc.CORNER)
-    grid_lat_corner = grid.get_coords(1, staggerloc=esmpy.StaggerLoc.CORNER)
-
-    grid_lon_corner[:] = lon[:, None] - 0.5 * dlon
-    grid_lat_corner[:, :-1] = lat[None, :] - 0.5 * dlat
-    grid_lat_corner[:, -1] = 90.0
-
-    return grid, lat, lon
-
-
-def build_my_grid(bounds, resolution):
-    """
-    Build an esmpy (ESMF) grid object in memory co-ords which align with the edge of the MOM6 mesh
-
-    """
-    epsilon = 1e-9 
-
-    lat = np.arange(bounds['lat_min'], bounds['lat_max'] + resolution - epsilon, resolution)
-
-    dlat = resolution   
-    dlon = dlat
-
-    nlat = len(lat)
-
-    if nlat % 2 != 0:
-        print (f'ERROR : Number of atmospheric latitude points is {nlat}')
-        print ('ERROR : Change resolution to ensure an even number of points within your ocean mesh bounds' )
-        sys.exit(1)
-
-    lon = np.arange(bounds['lon_min'], bounds['lon_max'] + resolution - epsilon, resolution)
-
-    nlon = len(lon)
-
-    if nlon % 2 != 0:
-        print (f'ERROR : Number of atmospheric longitude points is {nlon}')
-        print ('ERROR : Change resolution to ensure an even number of points within your ocean mesh bounds' )
-        sys.exit(1)
-
-    max_index = np.array([nlon, nlat])
-    grid = esmpy.Grid(max_index, num_peri_dims=1, staggerloc=[esmpy.StaggerLoc.CENTER])
-
-    grid.get_coords(0)[:] = lon[:, None]
-    grid.get_coords(1)[:] = lat[None, :]
-
-    # corner 
-    grid.add_coords([esmpy.StaggerLoc.CORNER])
-
-    grid_lon_corner = grid.get_coords(0, staggerloc=esmpy.StaggerLoc.CORNER)
-    grid_lat_corner = grid.get_coords(1, staggerloc=esmpy.StaggerLoc.CORNER)
-
-    grid_lon_corner[:] = lon[:, None] - 0.5 * dlon
-    grid_lat_corner[:, :-1] = lat[None, :] - 0.5 * dlat
-    grid_lat_corner[:, -1] = 90.0
-    
-    return grid, lat, lon
-
-
-def build_grid_cons(bounds, nlat, nlon):
+def build_grid_cons(bounds, res):
     """
     Build an esmpy (ESMF) grid object in memory using a modification 
     of Kieran's method at https://gist.github.com/kieranricardo/eb98f76235255efff800d28c2442e5c3
@@ -162,95 +82,54 @@ def build_grid_cons(bounds, nlat, nlon):
     lon0, lon1 = bounds['lon_min'],bounds['lon_max']
     lat0, lat1 = bounds['lat_min'],bounds['lat_max']
 
+    # Let's enforce the distance b/w the bounds is a multiple of the input resolution
+    # to prevent rounding errors
+
+    delta_lat = np.ptp([bounds['lat_min'],bounds['lat_max']])
+    delta_lon = np.ptp([bounds['lat_min'],bounds['lat_max']])
+
+    nlat = int(np.round(delta_lat/res))
+    nlon = int(np.round(delta_lon/res))
+
     max_index = np.array([nlon, nlat])
     grid = esmpy.Grid(max_index, 
                     coord_sys=esmpy.CoordSys.SPH_DEG,
                     staggerloc=[esmpy.StaggerLoc.CORNER])
 
 
-    # Get the coordinate arrays for the cell corners (vertices)
+    # Get the coordinate arrays for the cell vertices 
     grid_lon_c = grid.get_coords(0, staggerloc=esmpy.StaggerLoc.CORNER)
     grid_lat_c = grid.get_coords(1, staggerloc=esmpy.StaggerLoc.CORNER)
 
-    # Define the coordinates for the corners (vertices)
+    # Define the coordinates for the vertices 
     # The coordinate array size is now (nlat+1) x (nlog+1)
-    lat_corners = np.linspace(lat0, lat1, max_index[0] + 1)
-    lon_corners = np.linspace(lon0, lon1, max_index[1] + 1)
+    lat_vertices = np.linspace(lat0, lat1, max_index[0] + 1)
+    lon_vertices = np.linspace(lon0, lon1, max_index[1] + 1)
 
     # Assign coordinates to the grid object
     for j in range(max_index[0] + 1):
         for i in range(max_index[1] + 1):
-            grid_lat_c[j, i] = lat_corners[j]
-            grid_lon_c[j, i] = lon_corners[i]
+            grid_lat_c[j, i] = lat_vertices[j]
+            grid_lon_c[j, i] = lon_vertices[i]
 
-    return grid, lat_corners, lon_corners
-
-
-def regrid(atm_grid, ocn_mesh, ocn_mask, nlat, nlon, lats, lons):
-    """
-    Use the ESMF regridder to grid the ocn_mask onto the 
-    atm_grid
-    """
-    ocn_mesh, ocn_mask, bounds = load_ocn_data(ocean_file)
-
-    atm_field = esmpy.Field(atm_grid, meshloc=esmpy.api.constants.MeshLoc.ELEMENT)
-    atm_field.data[:] = 0.0    
-
-    ocn_field = esmpy.Field(ocn_mesh, meshloc=esmpy.api.constants.MeshLoc.ELEMENT)
-    ocn_field.data[:] = ocn_mask
-
-    ocn_to_atm_cons = esmpy.api.regrid.Regrid(
-      ocn_field, atm_field, 
-      unmapped_action=esmpy.api.constants.UnmappedAction.IGNORE,
-      regrid_method=esmpy.api.constants.RegridMethod.CONSERVE,
-      norm_type=esmpy.api.constants.NormType.DSTAREA, 
-      factors=True
-    )
-
-    new_ocn_frac = atm_field.data.reshape((nlat, nlon))
-
-    # Output to array
-    lat_coord = xr.DataArray(
-      dims=['lat'],
-      coords=dict(lat=lats),
-      data=lats,
-      attrs=dict(standard_name='latitude', units='degrees_north')
-    )
-
-    lon_coord = xr.DataArray(
-      dims=['lon'],
-      coords=dict(lon=lons),
-      data=lons,
-      attrs=dict(standard_name='longitude', units='degrees_east')
-     )
-
-    da = xr.DataArray(
-    data=new_ocn_frac,
-    dims=["lat", "lon"],
-    coords=dict(
-        lat=lats,
-        lon=lons,
-    ),
-    attrs=dict(um_stash_source='m01s00i505')
-    )
-
-    ds = xr.Dataset(dict(landfrac=da))
-
-    return ds
+    return grid, lat_vertices, lon_vertices
 
 
-def regrid_cons(grid,ocn_file,nlat, nlon, lat_corners,lon_corners):
+def regrid_cons(grid,
+                ocn_mesh,
+                ocn_mask,
+                bounds, 
+                lat_vertices,
+                lon_vertices):
     """
     Regrid my conservative grid object
     """
     src_field_conserve = esmpy.Field(grid, name='source_data_conserve')
 
     # Define the data on the centers of the cells (which is N_y x N_x)
-    src_lat_centers = (lat_corners[:-1] + lat_corners[1:]) / 2.0
-    src_lon_centers = (lon_corners[:-1] + lon_corners[1:]) / 2.0
+    src_lat_centers = (lat_vertices[:-1] + lat_vertices[1:]) / 2.0
+    src_lon_centers = (lon_vertices[:-1] + lon_vertices[1:]) / 2.0
     src_field_conserve.data[:] = 100.0 + np.outer(src_lat_centers, np.cos(np.deg2rad(src_lon_centers)))
-
-    ocn_mesh, ocn_mask, bounds = load_ocn_data(ocn_file)
 
     # Invert the mask
     ocn_mask = np.logical_not(ocn_mask).astype('int')
@@ -339,25 +218,17 @@ if __name__ == "__main__":
     ocean_file = args.ocean_file
     atm_res = float(args.atm_res)
 
-    out_fp = 'dummy_kieran.nc'
-    nlat=24
-    nlon=24
-
     ocn_mesh, ocn_mask, bounds = load_ocn_data(ocean_file)
-    atm_grid, lats, lons = build_grid(bounds, nlat)
-    ds = regrid(atm_grid, ocn_mesh, ocn_mask, nlat, nlon, lats, lons)
-    ds.to_netcdf(out_fp)
 
-    my_atm_grid, lats, lons = build_my_grid(bounds, atm_res)
-    nlon = 36
-    my_ds = regrid(my_atm_grid, ocn_mesh, ocn_mask, nlat, nlon, lats,
-    lons)
+    atm_grid_cons, lats, lons = build_grid_cons(bounds, 
+                                                atm_res)
 
-    out_fp = 'dummy_paul.nc'
-    my_ds.to_netcdf(out_fp)
+    con_ds = regrid_cons(atm_grid_cons, 
+                         ocn_mesh,
+                         ocn_mask,
+                         bounds, 
+                         lats, 
+                         lons)
 
-    atm_grid_cons, lats, lons = build_grid_cons(bounds, nlat, nlon)
-    con_ds = regrid_cons(atm_grid_cons, ocean_file, nlat, nlon, lats, lons)
-
-    out_fp = 'dummy_cons.nc'
+    out_fp = 'target_grid.nc'
     con_ds.to_netcdf(out_fp)
