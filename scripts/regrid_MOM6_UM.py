f4ec904abdeab979095e18b463c2ab9024fd7ffa
Paul Gregory <pag548@gadi-login-04.gadi.nci.org.au>
2026-05-12 20:47:42 +1000

    UPDATES : Final version of regrid_MOM6_UM.py

--- scripts/regrid_MOM6_UM.py
+++ scripts/regrid_MOM6_UM.py
@@ -19,10 +19,14 @@ def add_options(parser):
     """
     parser.description = 'Regrid a MOM6 ocean mesh onto a regular grid with the same bounds.\n The user specifies the output resolution and output file'
 
-    parser.add_argument('-i','--input-ocean-mesh-file', 
+    parser.add_argument('-m','--input-ocean-mask-file', 
         dest='mesh_file', 
         type=str, 
-        help="The rMOM6 esmpy mesh file, e.g. access-rom3-esmpymesh.nc")
+        help="The rMOM6 mask file, e.g. access-rom3-ESMFmesh.nc")
+    parser.add_argument('-i','--input-ocean-hgrid-file', 
+        dest='hgrid_file', 
+        type=str, 
+        help="The rMOM6 hgrid file, e.g. hgrid.nc")
     parser.add_argument('-o','--output-land-sea-mask-file', 
         dest='outfile', 
         type=str, 
@@ -33,23 +37,24 @@ def add_options(parser):
         help="The desired atmospheric resolution in degrees, e.g. 0.025")
 
 
-def load_ocn_data(ocn_mesh_fp):
+def load_ocn_data(ocn_mesh_fp,
+                  hgrid_fp):
     """
-    Load the rMOM6 ocean mesh file and return at ESMPY mesh object
+    Load the rMOM6 ocean mesh file and hgrid and return at ESMPY mesh object
     """
 
     mesh_ds = xr.load_dataset(
         ocn_mesh_fp
     )
 
-    bounds = get_bounds(mesh_ds)
+    bounds = get_bounds(hgrid_fp)
 
     ocn_mask = mesh_ds.elementMask.data
 
     # We need to logically invert this because MOM6 and the UM tread land/ocean 
     # points differently. i.e. MOM6 has ocean points as '1' (True), the UM has them
     # as '0' (False)
-    #ocn_mask = np.logical_not(ocn_mask).astype('int')
+    ocn_mask = np.logical_not(ocn_mask).astype('int')
 
     ocn_mesh = esmpy.api.mesh.Mesh(
         filename=ocn_mesh_fp, 
@@ -59,20 +64,22 @@ def load_ocn_data(ocn_mesh_fp):
     return ocn_mesh, ocn_mask, bounds
 
 
-def get_bounds(ocn_ds):
+def get_bounds(hgrid_fp):
     """
     Return the bounds of the the ocean mesh dataset
     """
-    coords = ocn_ds.nodeCoords.data
+    hgrid = xr.load_dataset(hgrid_fp)
 
-    # Find the min and max 
-    lons = coords[:,0]
-    lats = coords[:,1]
+    # Find the min and max of the hgrid's centre coords \
+    # where the mask is defined
 
-    bounds = { 'lat_min' : lats.min(), 
-               'lat_max' : lats.max(), 
-               'lon_min' : lons.min(), 
-               'lon_max' : lons.max() }
+    lons = hgrid.x[1::2,1::2]
+    lats = hgrid.y[1::2,1::2]
+
+    bounds = { 'lat_min' : lats.data.min(), 
+               'lat_max' : lats.data.max(), 
+               'lon_min' : lons.data.min(), 
+               'lon_max' : lons.data.max() }
 
     return bounds
 
@@ -97,52 +104,48 @@ def build_grid_cons(bounds,
     delta_lat = np.ptp([bounds['lat_min'],bounds['lat_max']])
     delta_lon = np.ptp([bounds['lat_min'],bounds['lat_max']])
 
-    nlat = int(np.round(delta_lat/res))
-    nlon = int(np.round(delta_lon/res))
+    nlat = int(np.round(delta_lat/res))+1
+    nlon = int(np.round(delta_lon/res))+1
 
     max_index = np.array([nlon, nlat])
     grid = esmpy.Grid(max_index, 
                     coord_sys=esmpy.CoordSys.SPH_DEG,
-                    staggerloc=[esmpy.StaggerLoc.CORNER])
-
-
-    # Get the coordinate arrays for the cell vertices (or corners)
-    grid_lon_v = grid.get_coords(0, staggerloc=esmpy.StaggerLoc.CORNER)
-    grid_lat_v = grid.get_coords(1, staggerloc=esmpy.StaggerLoc.CORNER)
-
-    # Define the coordinates for the vertices 
-    # The coordinate array size is now (nlat+1) x (nlog+1)
-    lat_vertices = np.linspace(lat0, lat1, max_index[0] + 1)
-    lon_vertices = np.linspace(lon0, lon1, max_index[1] + 1)
-
-    # Assign coordinates to the grid object
-    for j in range(max_index[0] + 1):
-        for i in range(max_index[1] + 1):
-            grid_lat_v[j, i] = lat_vertices[j]
-            grid_lon_v[j, i] = lon_vertices[i]
-
-    # Let's create a dual grid where we create add the 
-    # co-ordinates of the grid centers
-    grid.add_coords(staggerloc=esmpy.StaggerLoc.CENTER)
+                    staggerloc=[esmpy.StaggerLoc.CENTER])
 
     grid_lon_c = grid.get_coords(coord_dim=0, staggerloc=esmpy.StaggerLoc.CENTER)
     grid_lat_c = grid.get_coords(coord_dim=1, staggerloc=esmpy.StaggerLoc.CENTER)
 
     # Define the coordinates for the centers
-    lat_centres = np.linspace(lat0+res/2, lat1-res/2, max_index[0])
-    lon_centres = np.linspace(lon0+res/2, lon1-res/2, max_index[1])
+    lat_centres = np.linspace(lat0, lat1, max_index[0] )
+    lon_centres = np.linspace(lon0, lon1, max_index[1] )
 
-    # Assign coordinates to the grid object
+    # Assign coordinates to the grid object centres
     for j in range(max_index[0]):
         for i in range(max_index[1]):
             grid_lat_c[j, i] = lat_centres[j]
             grid_lon_c[j, i] = lon_centres[i]
 
-    return grid, lat_vertices, lon_vertices
+    # Now create vertices around this grid
+    grid.add_coords(staggerloc=esmpy.StaggerLoc.CORNER)
+    grid_lon_v = grid.get_coords(0, staggerloc=esmpy.StaggerLoc.CORNER)
+    grid_lat_v = grid.get_coords(1, staggerloc=esmpy.StaggerLoc.CORNER)
+
+    # Define the coordinates for the vertices
+    lat_vertices = np.linspace(lat0-res/2, lat1+res/2, max_index[0]+1)
+    lon_vertices = np.linspace(lon0-res/2, lon1+res/2, max_index[1]+1)
+
+    # Assign coordinates to the grid object corners
+    for j in range(max_index[0]+1):
+        for i in range(max_index[1]+1):
+            grid_lat_v[j, i] = lat_vertices[j]
+            grid_lon_v[j, i] = lon_vertices[i]
+
+    return grid, grid_lat_c, grid_lon_c
 
 
 def create_dataarray(field,
-                     index):
+                     index,
+                     binary=True):
     """
     Create a datarray for a specified field
     """
@@ -152,6 +155,11 @@ def create_dataarray(field,
     lats = coords[index][1]
 
     data = field.data
+
+    if binary:
+        # Force the field to be binary                            
+        data = xr.where(data > 0.5, 1,  data)
+        data = xr.where(data <= 0.5, 0, data)
 
     # Output to DataArray
     lat_coord = xr.DataArray(
@@ -202,8 +210,7 @@ def create_grid_mapping_variable():
     return grid_mapping_var
 
 
-def convert_gridded_field(field,
-                         outfilename):
+def convert_gridded_field(field):
     """
     Outputs a gridded ESMF 2-Dfield object to xarray dataset
     """
@@ -241,9 +248,6 @@ def convert_gridded_field(field,
                     ),
                     attrs=dict(Conventions='CF-1.7')
                     )
-
-                field_ds.to_netcdf(outfilename)
-                print(f"INFO : Writing {outfilename}")
     
     return field_ds
 
@@ -262,12 +266,14 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     MOM6_mesh_file = args.mesh_file
+    hgrid_file = args.hgrid_file
     target_mask_file = args.outfile
     atm_res = float(args.atm_res)
 
     #Load the Mesh file
     print(f"INFO : Opening {MOM6_mesh_file}")
-    MOM6_mesh, MOM6_mask, bounds = load_ocn_data(MOM6_mesh_file)
+    MOM6_mesh, MOM6_mask, bounds = load_ocn_data(MOM6_mesh_file,
+                                                 hgrid_file)
 
     # Create the MOM6 mask field
     MOM6_field = esmpy.Field(MOM6_mesh,meshloc=esmpy.MeshLoc.ELEMENT)
@@ -283,7 +289,7 @@ if __name__ == "__main__":
     # the grid centres
     UM_field = esmpy.Field(UM_grid,staggerloc=esmpy.StaggerLoc.CENTER)
 
-    # To use the conservative regridder, we are going to define a dual grid composed of the UM grid's cell centres
+    # Now use the conservative regridder
 
     MOM6_to_UM_cons = esmpy.api.regrid.Regrid(
       MOM6_field, 
@@ -294,21 +300,10 @@ if __name__ == "__main__":
       factors=True
     )
 
-    # Now we interpolate the UM field (defined at the grid centre) back to the vertices
-    UM_field_v = esmpy.Field(UM_grid,staggerloc=esmpy.StaggerLoc.CORNER)
-    
-    UM_c_to_v = esmpy.api.regrid.Regrid(
-      UM_field, 
-      UM_field_v, 
-      unmapped_action=esmpy.api.constants.UnmappedAction.IGNORE,
-      regrid_method=esmpy.api.constants.RegridMethod.BILINEAR,
-      norm_type=esmpy.api.constants.NormType.DSTAREA, 
-      factors=True
-    )
+    # Test zeros
+    #UM_field.data[:] = 1.0
 
     # To do -output both for testing sake
-    ds_c = convert_gridded_field(UM_field,
-                        'target_centres.nc')
-
-    ds_v = convert_gridded_field(UM_field_v,
-                        'target_corners.nc')
+    ds_c = convert_gridded_field(UM_field)
+    ds_c.to_netcdf(target_mask_file)
+    print(f"INFO : Writing {target_mask_file}")
